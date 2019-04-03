28de28720dd225ef23a79d8b5fa50395a0f68ec9
kangyonggan <kangyonggan@gmail.com>
2019-04-03 18:43:27 +0800

    干掉iView

--- src/views/index.vue
+++ src/views/index.vue
@@ -7,41 +7,10 @@
         left: 0;
         text-align: center;
     }
-
-    .index h1 {
-        height: 150px;
-    }
-
-    .index h1 img {
-        height: 100%;
-    }
-
-    .index h2 {
-        color: #666;
-        margin-bottom: 200px;
-    }
-
-    .index h2 p {
-        margin: 0 0 50px;
-    }
-
-    .index .ivu-row-flex {
-        height: 100%;
-    }
 </style>
 <template>
     <div class="index">
-        <Row type="flex" justify="center" align="middle">
-            <Col span="24">
-                <h1>
-                    <img src="https://raw.githubusercontent.com/iview/iview/master/assets/logo.png">
-                </h1>
-                <h2>
-                    <p>Welcome to your iView app!</p>
-                    <Button type="ghost" @click="handleStart">Start iView</Button>
-                </h2>
-            </Col>
-        </Row>
+        门户网站
     </div>
 </template>
 <script>
@@ -50,7 +19,7 @@
             handleStart() {
                 this.$Modal.info({
                     title: 'Bravo',
-                    content: 'Now, enjoy the convenience of iView.'
+                    content: 'Now, enjoy the convenience of Vue.'
                 });
             }
         }
